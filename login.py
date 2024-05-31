ffe018316b65451ed11869689f3a06b59b1df8c5
wanqian-windrunner <2287317341@qq.com>
2024-05-31 15:31:35 +0800

    微调json login

--- login.py
+++ login.py
@@ -7,17 +7,21 @@ class LoginSystem:
     def login(self):
         self.username = input("input your username: ")
         self.password = input("input your password:")
-        print(self.username,self.password)
+        # print(self.username,self.password)
         with open('.\data.json', 'r') as file:
             data = json.load(file)
-            # print(file)
-            print(data['users'])
-            print(data['users'][self.username]['password'] )
-            if self.username in data and data['users'][self.username]['password'] == self.password:
-                print("Login successful!")
-                self.login_status = True
-            else:
-                print("Invalid username or password.")
+        user_data = data['users']
+        print(user_data[self.username]['password'] )
+        print(self.password)
+        print(type(user_data[self.username]['password']))
+        if self.username in user_data and user_data[self.username]['password'] == self.password:
+            print("Login successful!")
+            self.login_status = True
+        else:
+            print("Invalid username or password.")
 
-login = LoginSystem()
-login.login()
+
+if __name__ == '__main__':
+    login = LoginSystem()
+    login.login()
+    

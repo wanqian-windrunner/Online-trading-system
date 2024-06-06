fb21d075ec125a200f5fde41005727ad355dd66b
wanqian-windrunner <2287317341@qq.com>
2024-06-07 01:44:09 +0800

    整合目前的部分功能

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from time import sleep
 from menu import Menu
+from login import LoginSystem
 
+loginsystem=LoginSystem()
+# 登陆账户，然后存储用户数据
+while not loginsystem.login_status:
+    option = input("Choose your option:\n\t1.register\n\t2.login\n")
+    if option == '1':    
+        loginsystem.register()
+    elif option == '2':
+        loginsystem.login()
+    sleep(2)
+userdata = loginsystem.userdata
 
+print(userdata)
+sleep(2)
+# 然后展示菜单
 
-menu = Menu()
-menu.login_menu()
+from menu import Menu
+menusystem = Menu(loginsystem.login_status,userdata)
+menusystem.function_menu()

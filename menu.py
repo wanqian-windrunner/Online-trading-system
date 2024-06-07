e4a69b62ea12590de29d5f81e04301620f4c1a64
qklsuzuran <1303795362@qq.com>
2024-06-07 22:46:52 +0800

    qkl

--- menu.py
+++ menu.py
@@ -6,6 +6,7 @@ class Menu:
         self.logged_in = status
         self.balance = userdata['balance']
         self.goodsdata = Data_process.read()['goods']
+        self.bag = userdata['bag']
 
     def function_menu(self):
 
@@ -17,6 +18,7 @@ class Menu:
             print("5. Logout")
 
             choice = input("Enter your choice number: ")
+            print()
 
             if choice == "1":
                 # Logic for viewing account balance
@@ -26,7 +28,7 @@ class Menu:
                 print("Buying stocks...\n")
                 self.buy_stocks_menu()
             elif choice == "3":
-                print('no bag\n')
+                print(str(self.bag)+'\n')
             elif choice == "4":
                 # Logic for selling stocks
                 print("Selling stocks...\n")
@@ -58,11 +60,11 @@ class Menu:
         elif good_number == '1':
             print('\nprice: '+str(good['1']['price'])+'$')
             print('count: '+str(good['1']['count'])+'\n')
-            self.buy_time()
+            self.buy_time(good,good_number)
         elif good_number == '2':
             print('\nprice: '+str(good['2']['price'])+'$')
             print('count: '+str(good['2']['count'])+'\n')
-            self.buy_time()
+            self.buy_time(good,good_number)
         elif good_number == '3':
             print('\nprice: '+str(good['3']['price']+'$'))
             print('count: '+str(good['3']['count'])+'\n')
@@ -71,15 +73,23 @@ class Menu:
             print()
             self.function_menu()
 
-    def buy_time(self):
+    def buy_time(self,good,good_number):
+        good_number = int(good_number)
         print('1.Buy')
         print('2.Back\n')
         buy_choice = input('Please input your choice: ')
         print()
         if buy_choice == '1':
-            buy_num = input('\nPlease input how many you want to buy: ')
-            #背包未实装，故不写接下来的减法
+            buy_num = int(input('\nPlease input how many you want to buy: '))
+            if buy_num > int(good[str(good_number)]['count']):
+                print('Too many!\n')
+                self.buy_time(good,good_number)
             print('OK!')
+
+            good[str(good_number)]['count'] -= buy_num
+            self.balance -= buy_num * good[str(good_number)]['price']  #减钱的
+            self.bag[self.change(good_number)] += buy_num   #算数量的
+
             input('Press Enter to continue...\n')
             self.buy_stocks_menu()
         elif buy_choice == '2':
@@ -88,6 +98,17 @@ class Menu:
             print('Invalid choice. Please try again.\n')
 
 
+    def change(self,good_number):   #这个函数是把数字编号换成商品的
+        if good_number == 1:
+            return 'Apple'
+        elif good_number == 2:
+            return 'Banana'
+        elif good_number == 3:
+            return 'Fish'
+
+
+
 if __name__ == '__main__':
     a = Menu(True)
     a.function_menu()
+

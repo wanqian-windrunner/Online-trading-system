b7f093baf0be2843c1c324c8685e1c6dcb6caec7
wanqian-windrunner <2287317341@qq.com>
2024-06-22 22:41:35 +0800

    封装商品显示列表‘

--- utils.py
+++ utils.py
@@ -8,3 +8,12 @@ class Data_process:
     def write(data):
         with open('data.json', 'w') as file:
             json.dump(data, file, indent=2)
+
+def show_goods():
+    goods = Data_process.read()['goods']
+    for good in goods:
+        print(good+'.',goods[good]['name'], ' price:',goods[good]['price'],' count:',goods[good]['count'])
+
+
+if __name__ == '__main__':
+    show_goods()

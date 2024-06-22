479e7d11a7ff19b87af08bffda2021983bcd298c
wanqian-windrunner <2287317341@qq.com>
2024-06-22 20:26:45 +0800

    增加注释

--- login.py
+++ login.py
@@ -4,46 +4,63 @@ from utils import Data_process
 class LoginSystem:
     def __init__(self):
         self.login_status = False
+        # 读取所有数据
         self.totaldata = Data_process.read()
+        # 提取用户数据
         self.user_data = self.totaldata['users']
 
     def login(self):
-        # 先是输入用户名和密码
-        self.username = input("input your username: ")
-        self.password = input("input your password:")
-        # print(self.username,self.password)
+        # 输入用户名和密码
+        self.username = input("请输入您的用户名: ")
+        self.password = input("请输入您的密码: ")
+        # 打印输入的用户名和密码 (用于调试)
+        # print(self.username, self.password)
         
-        # 然后判断用户名和密码是否正确，正确则存储数据，否则提示错误
+        # 判断用户名和密码是否正确，正确则存储数据，否则提示错误
         if self.username in self.user_data and self.user_data[self.username]['password'] == self.password:
-            print("Login successful!Welcome, "+self.username+"!")
+            print("登录成功！欢迎, " + self.username + "!")
+            # 存储用户数据
             self.userdata = self.user_data[self.username]
             self.login_status = True
         else:
-            print("Invalid username or password.")
+            print("无效的用户名或密码。")
+
     def register(self):
-        self.username = input("Input your username: ")
+        # 输入新用户名
+        self.username = input("请输入您的用户名: ")
+        # 检查用户名是否已存在
         if self.username in self.user_data:
-            print("Repeated name!Try a new name or login.")
+            print("用户名已存在！请尝试一个新名字或登录。")
             return 0
-        self.password = input("Input your password:")  
-        if input("Please input your password again:") != self.password:
-            print('Sorry,goodbye~')
+        # 输入密码
+        self.password = input("请输入您的密码: ")  
+        # 确认密码是否一致
+        if input("请再次输入您的密码: ") != self.password:
+            print('抱歉，再见~')
             return 0
-        self.balance = input("Input your balance:")
-        self.userdata={'name':self.username,
-                        'password':self.password,
-                        'balance':self.balance,
-                        'bag':[]}
-        self.totaldata['users'][self.username]=self.userdata
+        # 输入余额
+        self.balance = input("请输入您的余额: ")
+        # 创建用户数据字典
+        self.userdata = {
+            'name': self.username,
+            'password': self.password,
+            'balance': self.balance,
+            'bag': []
+        }
+        # 将新用户数据添加到总数据中
+        self.totaldata['users'][self.username] = self.userdata
+        # 将总数据写回文件
         Data_process.write(self.totaldata)
         self.login_status = True
         
     def logout(self):
+        # 退出程序
         exit(0)
 
-
 if __name__ == '__main__':
+    # 创建登录系统实例
     login = LoginSystem()
+    # 执行登录
     login.login()
+    # 打印用户数据 (用于调试)
     print(login.userdata)
-    

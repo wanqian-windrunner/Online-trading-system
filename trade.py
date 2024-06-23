a599331ecf8ce4227db9f684f03023d435183bd8
wanqian-windrunner <2287317341@qq.com>
2024-06-23 12:22:32 +0800

    封装购买出售

---
+++ trade.py
@@ -0,0 +1,35 @@
+from utils import Data_process
+
+class Trade:
+    def __init__(self,user_data):
+        self.user_data = user_data
+
+    def purchase(self,good_id:str,quantity:int):
+        self.goods_data = Data_process.read()['goods']
+        # 添加到背包
+        self.user_data["bag"][good_id] += quantity
+        # 扣除钱包余额
+        self.user_data["balance"] -= self.goods_data[good_id]["price"]*quantity
+        # 减少商品
+        self.goods_data[good_id]["count"] -= quantity
+
+        # 写入json文件
+        total_data = Data_process.read()
+        total_data['users'][self.user_data['name']]=self.user_data
+        total_data['goods']=self.goods_data
+        Data_process.write(total_data)
+
+    def sell(self,good_id:str,quantity:int):
+        self.goods_data = Data_process.read()['goods']
+        # 从背包拿出
+        self.user_data["bag"][good_id] -= quantity
+        # 增加钱包余额
+        self.user_data["balance"] += self.goods_data[good_id]["price"]*quantity//2
+        # 增加商品
+        self.goods_data[good_id]["count"] += quantity
+
+        # 写入json文件
+        total_data = Data_process.read()
+        total_data['users'][self.user_data['name']]=self.user_data
+        total_data['goods']=self.goods_data
+        Data_process.write(total_data)

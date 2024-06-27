a222727a1519f9d4140dd3adf4863eec326f7f5a
wanqian windrunner <2287317341@qq.com>
2024-06-27 09:59:14 +0800

    都他妈跟你们说了，main我来写我来写，怎么就是不听呢

--- main.py
+++ main.py
@@ -21,34 +21,3 @@
 # from menu import Menu
 # menusystem = Menu(loginsystem.login_status,userdata)
 # menusystem.function_menu()
-
-
-from login import LoginSystem
-from utils import Data_process
-
-# 初始化数据
-Data_process.data_init()
-
-# 创建登录系统实例
-loginsystem = LoginSystem()
-
-# 登陆账户，然后存储用户数据
-while not loginsystem.login_status:
-    option = input("Choose your option:\n\t1.register\n\t2.login\n")
-    if option == '1':
-        loginsystem.register()
-    elif option == '2':
-        loginsystem.login()
-
-# 获取用户数据
-userdata = loginsystem.userdata
-print(userdata)
-
-# 延迟导入Menu类
-from menu import Menu
-
-# 展示菜单
-menusystem = Menu(loginsystem.login_status, userdata)
-menusystem.function_menu()
-
-
